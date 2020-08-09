eeeb1903f8cae4ffdf62d1fd32161fe7b8a888b4
Christopher Chu <christopherchu081098@gmail.com>
2020-08-09 15:34:31 -0400

    Wrote the code and added the tests for question 3

--- src/p3_replace_spaces.java
+++ src/p3_replace_spaces.java
@@ -1,4 +1,85 @@
-
 public class p3_replace_spaces {
+	
+	
+	/*
+	 * Problem Statement: Write a method to replace all spaces in a string with %20. 
+	 * You may assume that the string has sufficient space to hold the additional characters,
+	 * and you are given the "true" length of the string. If implementing in Java, please use a 
+	 * character array.
+	 */
+	
+	//Here is my implementation
+	//Let's say that I am give the string "chris chu owns this channel"
+	//The following array will look like this:
+	// |c| |h| |r| |i| |s| | - | |c| |h| |u| | - | |o| |w| |n| |s| | - | |t| |h| |i| |s| | - | |c| |h| |a| |n| |n| |e| |l|
+	//My method is as follows
+	// 1. count the number of spaces in the array 
+	// 2.If a space is going to be replaced by %20, this means the space has to be 2 more
+	// 3. Count the total number of characters
+	// 4. Make an array of total characters + 2 * the number of spaces in the array 
+	// 5. Iterate through string, keep a pointer for the array, and put the corresponding spaces in there. 
+	// 6. convert each character array to a string 
+	// The results should be chris%20chu%20owns%20this%20channel
+	public static String my_implementation(String str_to_convert) {
+		int num_spaces = 0;
+		for(int character = 0; character < str_to_convert.length(); character++) {
+			if (str_to_convert.charAt(character) == ' ') {
+				num_spaces++;
+			}
+		}
+		int extra_indices = 2 * num_spaces;
+		int new_array_size = str_to_convert.length() + extra_indices;
+		char[] new_string_array = new char[new_array_size];
+		int new_array_pointer = 0;
+		for(int character = 0; character < str_to_convert.length(); character++) {
+			if (str_to_convert.charAt(character) == ' ') {
+				new_string_array[new_array_pointer] = '%';
+				new_string_array[new_array_pointer + 1] = '2';
+				new_string_array[new_array_pointer + 2] = '0';
+				new_array_pointer += 3;
+			} else {
+				new_string_array[new_array_pointer] = str_to_convert.charAt(character);
+				new_array_pointer++;
+			}
+		}
+		StringBuilder final_string_builder = new StringBuilder();
+		for(int character = 0; character < new_string_array.length; character++) {
+			final_string_builder.append(new_string_array[character]);
+		}
+		return final_string_builder.toString();
+	}
+	
+	public static int count_of_char(char[] str, int start, int end, int target) {
+		int count = 0;
+		for(int index = start; index < end; index++) {
+			if(str[index] == target) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static String gayle_implementation(char[] str, int true_length) {
+		int number_of_spaces = count_of_char(str, 0, true_length, ' ');
+		int new_index = true_length - 1 + number_of_spaces * 2;
+		if(new_index + 1 < str.length) str[new_index + 1] = '\0';
+		for(int old_index = true_length - 1; old_index >= 0; old_index--) {
+			if(str[old_index] == ' ') {
+				str[new_index] = '0';
+				str[new_index - 1] = '2';
+				str[new_index - 2] = '%';
+				new_index -= 3;
+			} else {
+				str[new_index] = str[old_index];
+				new_index -= 1;
+			}
+		}
+		StringBuilder final_string_builder = new StringBuilder();
+		for(int character = 0; character < str.length; character++) {
+			final_string_builder.append(str[character]);
+		}
+		return final_string_builder.toString();
+	}
 
+	
 }
--- src/test_p3_replace_spaces.java
+++ src/test_p3_replace_spaces.java
@@ -1,9 +1,59 @@
 
 public class test_p3_replace_spaces {
-
+	
+	
+	public static boolean test_my_implementation_success() {
+		boolean checker = p3_replace_spaces.my_implementation("chris chu owns this channel").equals("chris%20chu%20owns%20this%20channel");
+		return checker;
+	}
+	
+	public static boolean test_my_implementation_failure() {
+		boolean checker = p3_replace_spaces.my_implementation("chris chu owns this channel").equals("chris%20chu%20owns%20this%2channel");
+		return checker;
+	}
+	
+	public static boolean test_gayle_implementation_success() {
+		String test_string = "i love gayle! :)";
+		char[] string_array = test_string.toCharArray();
+		String benchmark = "%20love%20gayle!";
+		boolean checker = p3_replace_spaces.gayle_implementation(string_array, string_array.length) == benchmark;
+		System.out.println(p3_replace_spaces.gayle_implementation(string_array, string_array.length));
+		return checker;
+	}
+	
+	public static boolean test_gayle_implementation_failure() {
+		String test_string = "i love gayle! :)";
+		char[] string_array = test_string.toCharArray();
+		String benchmark = "i%20love%20gayle!20:)";
+		boolean checker = p3_replace_spaces.gayle_implementation(string_array, string_array.length) == benchmark;
+		return checker;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num_passed = 0;
+		int num_failed = 0;
+		if(test_my_implementation_success()) {
+			num_passed++;
+		} else {
+			num_failed++;
+		}
+		if(!test_my_implementation_failure()) {
+			num_passed++;
+		} else {
+			num_failed++;
+		}
+		/*if(test_gayle_implementation_success()) {
+			num_passed++;
+		} else {
+			num_failed++;
+		}
+		if(!test_gayle_implementation_failure()) {
+			num_passed++;
+		} else {
+			num_failed++;
+		}*/
+		System.out.println("TEST RESULTS: " + num_passed + " TESTS PASSED, " + num_failed + " TESTS FAILED.");
 	}
 
+	
 }
