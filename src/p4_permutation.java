7964c371ffb2bc8dee2eb76ce7b3697fc4477181
Author Name <chrischucodes@gmail.com>
2020-08-10 11:41:36 -0400

    making a new class and file for p4 permutation

---
+++ src/p4_permutation.java
@@ -0,0 +1,92 @@
+
+public class p4_permutation {
+	
+	
+	/*
+	 * Given a string, write a function to check if
+	 *  it is a permutation of a palindrome. 
+	 */
+
+	//Solution #1 
+	public static int getCharNumber1(Character c) {
+		int a = Character.getNumericValue('a');
+		int z = Character.getNumericValue('z');
+		int val = Character.getNumericValue(c);
+		if(a <= val && val <= z) {
+			return val - a;
+		}
+		return -1;
+	}
+	
+	public static int[] buildCharFrequencyTable1(String phrase) {
+		int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+		for(char c: phrase.toCharArray()) {
+			int x = getCharNumber1(c);
+			if(x != -1) table[x]++;
+		}
+		return table;
+	}
+	
+	public static boolean checkMaxOneOdd1(int[] table) {
+		boolean foundOdd = false;
+		for(int count : table) {
+			if(count % 2 == 1) {
+				if(foundOdd) {
+					return false;
+				}
+				foundOdd = true;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean isPermutationOfPalindrome1(String phrase) {
+		int[] table = buildCharFrequencyTable1(phrase);
+		return checkMaxOneOdd1(table);
+	}
+	
+	//Solution #2
+	public static boolean isPermutationOfPalindrome2(String phrase) {
+		int countOdd = 0;
+		int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+		for(char c : phrase.toCharArray()) {
+			int x = getCharNumber1(c);
+			if(x != 1) {
+				if(table[x] % 2 == 1) {
+					countOdd++;
+				}
+			} else {
+				countOdd--;
+			}
+		}
+		return countOdd <= 1;
+	}
+	
+	//Solution #3
+	public static int toggle3(int bitVector, int index) {
+		if(index < 0) return bitVector;
+		int mask = 1 << index;
+		bitVector ^= mask;
+		return bitVector;
+	}
+	
+	public static int createBitVector3(String phrase) {
+		int bitVector = 0;
+		for(char c: phrase.toCharArray()) {
+			int x = getCharNumber1(c);
+			bitVector = toggle3(bitVector, x);
+		}
+		return bitVector;
+	}
+	
+	public static boolean checkAtMostOneBitSet3(int bitVector) {
+		return (bitVector & (bitVector - 1)) == 0;
+	}
+	
+	public static boolean isPermutationOfPalindrome3(String phrase) {
+		int bitVector = createBitVector3(phrase);
+		return checkAtMostOneBitSet3(bitVector);
+	}
+	
+
+}
---
+++ src/test_p4_permutation.java
@@ -0,0 +1,65 @@
+
+public class test_p4_permutation {
+	
+	
+	public static boolean test_isPermutationOfPalindrome1_success() {
+		return p4_permutation.isPermutationOfPalindrome1("racecar");
+	}
+	
+	public static boolean test_isPermutationOfPalindrome1_failure() {
+		return p4_permutation.isPermutationOfPalindrome1("gameofthrones");
+	}
+	
+	public static boolean test_isPermutationOfPalindrome2_success() {
+		return p4_permutation.isPermutationOfPalindrome1("racecar");
+	}
+	
+	public static boolean test_isPermutationOfPalindrome2_failure() {
+		return false;
+	}
+	
+	public static boolean test_isPermutationOfPalindrome3_success() {
+		return p4_permutation.isPermutationOfPalindrome1("racecar");
+	}
+	
+	public static boolean test_isPermutationOfPalindrome3_failure() {
+		return false;
+	}
+
+	public static void main(String[] args) {
+		int numPassed = 0, numFailed = 0;
+		if(test_isPermutationOfPalindrome1_success()) {
+			numPassed++;
+		}else {
+			numFailed++;
+		}
+		if(!test_isPermutationOfPalindrome1_failure()) {
+			numPassed++;
+		} else {
+			numFailed++;
+		}
+		if(test_isPermutationOfPalindrome2_success()) {
+			numPassed++;
+		} else {
+			numFailed++;
+		}
+		if(!test_isPermutationOfPalindrome2_failure()) {
+			numPassed++;
+		} else {
+			numFailed++;
+		}
+		if(test_isPermutationOfPalindrome3_success()) {
+			numPassed++;
+		} else {
+			numFailed++;
+		}
+		if(!test_isPermutationOfPalindrome3_failure()) {
+			numPassed++;
+		} else {
+			numFailed++;
+		}
+		System.out.println("TEST RESULTS: " + numPassed + " TESTS PASSED, " + numFailed + " TESTS FAILED.");
+	}
+
+	
+}
