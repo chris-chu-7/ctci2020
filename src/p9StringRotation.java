c0f86952e20b753ffe18e7348cf2cd8740718424
= <=>
2020-08-11 22:02:35 -0400

    Finished all the main classes

--- src/p9StringRotation.java
+++ src/p9StringRotation.java
@@ -1,4 +1,15 @@
 
 public class p9StringRotation {
 
+	
+	public static boolean isRotation_1(String s1, String s2) {
+		int len = s1.length();
+		if(len == s2.length() && len > 0) {
+			String combined = s1 + s1;
+			return combined.contains(s2);
+		}
+		return false;
+	}
+	
+	
 }
