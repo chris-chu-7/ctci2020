b3c3926dc59582de3b3df6918ed8d52f612dc586
= <=>
2020-08-29 14:24:48 -0400

    Check Sub Tree Alternative 4.10

--- src/p34CheckSubTreeAlternative.java
+++ src/p34CheckSubTreeAlternative.java
@@ -1,4 +1,24 @@
 
 public class p34CheckSubTreeAlternative {
 
+	
+	public boolean matchTree(TreeNodeCTCI r1, TreeNodeCTCI r2) {
+		if(r1 == null && r2 == null) return true;
+		else if(r1 == null || r2 == null) return false;
+		else if(r1.data != r2.data) return false;
+		return matchTree(r1.left, r2.left) && matchTree(r1.right, r2.right);
+	}
+	
+	public boolean subTree(TreeNodeCTCI r1, TreeNodeCTCI r2) {
+		if(r1 == null) return false;
+		else if(r1.data == r2.data && matchTree(r1, r2)) return true;
+		return subTree(r1.left, r2) || subTree(r1.right, r2);
+	}
+	
+	public boolean containsTree(TreeNodeCTCI t1, TreeNodeCTCI t2) {
+		if(t2 == null) return true;
+		return subTree(t1, t2);
+	}
+	
+	
 }
