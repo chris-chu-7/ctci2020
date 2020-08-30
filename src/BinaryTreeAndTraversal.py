f9b7c12be95bf8978983fe31ced6fdfae2ea701e
= <=>
2020-08-30 14:00:32 -0400

    binary tree and traversal

---
+++ src/BinaryTreeAndTraversal.py
@@ -0,0 +1,46 @@
+class Node(object):
+    
+    
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+        
+
+class BinaryTree(object):
+    
+    
+    def __init__(self, root):
+        self.root = Node(root)
+        
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root,"")
+        elif traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
+        elif traversal_type == "postorder":
+            return self.postorder_print(tree.root, "")
+        else:
+            print("traversal type " + str(traversal) + " is not supported.")
+            
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal += self.preorder_print(start.left, traversal)
+            traversal += self.preorder_print(start.right, traversal)
+        return traversal
+        
+    def inorder_print(self, start, traversal):
+        if start:
+            traversal += self.inorder_print(start.left, traversal)
+            traversal += (str(start.value) + "-")
+            traversal += self.inorder_print(start.right, traversal)
+        return traversal
+        
+    def postorder_print(self, start, traversal):
+        if start: 
+            traversal += self.postorder_print(start.left, traversal)
+            traversal += self.postorder_print(start.right, traversal)
+            traversal += (str(start.value) + "-")
+    
