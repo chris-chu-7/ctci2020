47f0ab54c4612a84d2324ad2da36ae42af3bee44
= <=>
2020-09-03 15:29:58 -0400

    the heavy pill class

---
+++ src/p45TheHeavyPill.java
@@ -0,0 +1,18 @@
+
+public class p45TheHeavyPill {
+	
+	
+	//20 bottles of pills, 19 1.0 gram pills, one 1.1 gram pill.
+	//Given a scale providing exact measurement, how would you find
+	//the heavy bottle. 
+	
+	//take 1 pill from bottle 1, 2 pills from bottle 2, etc. 
+	//weigh the mix of pills. 
+	//do this equation
+	//1 + 2 + 3 + 4 + ...+ 20 = 210.
+	//do x - 210/0.1 grams. this will show you.
+	//if the bottles weighed 211.3 grams, then 1.3/0.1 = bottle 13
+	//will have the pills with the slightly extra weight. 
+	
+	
+}
