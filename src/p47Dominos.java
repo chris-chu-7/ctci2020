b7499f3ad4fe903c695fbdfc1fa5d6862edf6ba0
= <=>
2020-10-03 13:31:17 -0400

    p47 dominos

---
+++ src/p47Dominos.java
@@ -0,0 +1,9 @@
+
+public class p47Dominos {
+	
+	//a chessboard has 32 white and black squares.
+	//Take away the edges, you take away 2 of the same square, which leaves 30 of one 
+	//square and 32 of the other square
+	//domino cover one of each square. Hence, this proposed proposition to cover the entire board is impossible.
+
+}
