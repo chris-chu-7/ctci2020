b42de7f6554f332ede70dca513b3c6bb5d7ead69
= <=>
2020-12-12 13:17:35 -0500

    Update p1Colorland.py

--- src/p1Colorland.py
+++ src/p1Colorland.py
@@ -20,12 +20,72 @@ def getColor(color):
         return 5
     else: 
         raise Exception("Must be a valid color")
+   
+        
+def getNumArray(board):
+    newBoard = []
+    for color in range(0,len(board)):
+        newBoard.append(getColor(board[color]))
+    return newBoard
+
+
+def emptyBoard(boardLen):
+    zeroBoard = []
+    for i in range(0, boardLen + 1):    
+        zeroBoard.append(0)
+    return zeroBoard
+
+        
         
 
 def Colorland(numSquares, board):
-    print(board)
+    boardArray = getNumArray(board)
+    finalBoard = emptyBoard(len(board))
+    b = [0, 0]
+    o = [0, 0]
+    p = [0, 0]
+    g = [0, 0]
+    r = [0, 0]
+    y = [0, 0]
+    
+    for element in range(0, len(board)):
+        color = boardArray[element]
+        if(color == 0):
+            b[0] = element
+            b[1] = b[1] + 1
+            if(element == len(board) - 1):
+                return b[1]
+        elif(color == 1):
+            o[0] = element
+            o[1] = o[1] + 1
+            if(element == len(board) - 1):
+                return o[1]
+        elif(color == 2):
+            p[0] = element
+            p[1] = p[1] + 1
+            if(element == len(board) - 1):
+                return p[1]
+        elif(color == 3):
+            g[0] = element
+            g[1] = g[1] + 1
+            if(element == len(board) - 1):
+                return g[1]
+        elif (color == 4):
+            r[0] = element
+            r[1] = r[1] + 1
+            if(element == len(board) - 1):
+                return r[1]
+        elif(color == 5):
+            y[0] = element
+            y[1] = y[1] + 1
+            if(element == len(board) - 1):
+                return y[1]
+        else: 
+            raise Exception("Must be a valid color")
+    
+    
     
 
 if __name__ == "__main__":
-    board = ["Blue", "Orange", "Pink", "Green"]
-    Colorland(len(board), board)
+    board = ["Blue", "Orange", "Pink", "Green", "Red", "Yellow", "Yellow", "Yellow", "Yellow"]
+    print(Colorland(len(board), board))
