346005c36057789f55b10e95be343a32ba0a7947
= <=>
2020-12-12 12:45:09 -0500

    Create p1Colorland.py

---
+++ src/p1Colorland.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 12:35:27 2020
+
+@author: cchu3
+"""
+
+def Colorland(numSquares, board):
+    print(board)
+    
+
+if __name__ == "__main__":
+    board = ["Blue", "Orange", "Pink", "Green"]
+    Colorland(len(board), board)
