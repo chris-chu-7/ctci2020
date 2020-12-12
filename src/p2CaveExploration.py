4a0f9cef72c94bbcdd930af6ced85ffce8f6d2ca
= <=>
2020-12-12 16:12:21 -0500

    Update p2CaveExploration.py

--- src/p2CaveExploration.py
+++ src/p2CaveExploration.py
@@ -5,8 +5,87 @@ Created on Sat Dec 12 13:35:06 2020
 @author: cchu3
 """
 
+'''
+This is the main function, returning the amount of time it takes for the Goldfish to reach "home",
+Or the rightmost corner of the square array. 
+'''
+
+import sys
+
 def caveExploration(N, rockHeights):
-        return 0
+        rocks = rockHeighttoInt(rockHeights)
+        water = rockHeighttoInt(rockHeights)
+        row = 0
+        column = 0
+        level = 0
+        hours = 0
+        return dfs(rocks, water, row, column, level, hours)
+
+'''
+This function converts the rock height array into an input array, so that depth-first search can be 
+subsequently implemented. 
+'''    
+def rockHeighttoInt(rockHeights):
+    for i in range(0, len(rockHeights)):
+        for j in range(0, len(rockHeights[0])):
+            rockHeights[i][j] = int(rockHeights[i][j])
+    return rockHeights
+
+
+'''
+This function increments the water level by 1 every hour.
+Subject to change if this abides by physics (recursion then.)
+'''
+def incrementWaterHeight(waterLevel):
+    for i in range(0, len(waterLevel)):
+        for j in range(0, len(waterLevel[0])):
+            waterLevel[i][j] += 1
+    return waterLevel
+
+
+'''
+This function performs dfs to have the goldfish traverse through this path.
+The change is that the water level increases with the number of hours.
+'''
+def dfs(rocks, water, row, column, level, hours):
+    
+    min_hours = sys.maxsize 
+
+    #base case
+    if(row == len(rocks) and column == len(rocks[0])):
+        return level
+    
+    
+    #up
+    if((row - 1) >= 0):
+        if(rocks[row - 1][column] < rocks[row][column] or water[row - 1][column] < rocks[row][column]):
+            updfs = dfs(rocks, incrementWaterHeight(water), row - 1, column, level + 1, hours + 1)
+            min_hours = min(min_hours, updfs)
+        
+    
+    #down
+    if((row + 1) < len(rocks)):
+        if(rocks[row + 1][column] < rocks[row][column] or water[row + 1][column] < rocks[row][column]):
+            downdfs = dfs(rocks, incrementWaterHeight(water), row + 1, column, level + 1, hours + 1)
+            min_hours = min(min_hours, downdfs)
+
+    
+    #left
+    if((column - 1) >= 0):
+        if(rocks[row][column - 1] < rocks[row][column] or water[row][column - 1] < rocks[row][column]):
+            leftdfs = dfs(rocks, incrementWaterHeight(water), row, column - 1, level + 1, hours + 1)
+            min_hours = min(min_hours, leftdfs)
+
+    
+    #right
+    if((column + 1) < len(rocks[0])):
+        if(rocks[row][column + 1] < rocks[row][column] or water[row][column + 1] < rocks[row][column]):
+            rightdfs = dfs(rocks, incrementWaterHeight(water), row, column + 1, level + 1, hours + 1)
+            min_hours = min(min_hours, rightdfs)
+            
+    #recursive case
+    return min_hours
+
 
 if __name__ == "__main__":
     #test 1
@@ -18,6 +97,7 @@ if __name__ == "__main__":
         print("Case 1 Wrong. Result: " + str(caveExploration(N1, rockHeights1)) + " Expected: " + "4")
     
     #test 2
+    
     N2 = 5
     rockHeights2 = [['0', '2', '3', '10', '4'], 
                     ['10', '23', '2', '21', '12'],
