f739de08a3c696c83d63a1b3808c3da9528f5571
= <=>
2020-12-12 12:50:11 -0500

    Update p1Colorland.py

--- src/p1Colorland.py
+++ src/p1Colorland.py
@@ -5,6 +5,23 @@ Created on Sat Dec 12 12:35:27 2020
 @author: cchu3
 """
 
+def getColor(color):
+    if(color == "Blue"):
+        return 0
+    elif(color == "Orange"):
+        return 1
+    elif(color == "Pink"):
+        return 2
+    elif(color == "Green"):
+        return 3
+    elif (color == "Red"):
+        return 4
+    elif(color == "Yellow"):
+        return 5
+    else: 
+        raise Exception("Must be a valid color")
+        
+
 def Colorland(numSquares, board):
     print(board)
     
